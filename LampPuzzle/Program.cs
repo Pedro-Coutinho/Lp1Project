fd2f2fb09e1a848b6544dc82f9075dc8a6e6a01e
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-04-13 15:24:10 +0100

    Add main game logic

--- LampPuzzle/Program.cs
+++ LampPuzzle/Program.cs
@@ -6,7 +6,73 @@ namespace LampPuzzle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool[] lampStatus = new bool[3];
+            bool victory = false;
+            int lampsOn = 0;
+
+            int input = 0;
+
+            PrintLamps(lampStatus);
+
+            for (int i = 0; i < 6; i++)
+            {
+                Int32.TryParse(Console.ReadLine(), out input);
+                bool tempLamp = false;
+                switch (input)
+                {
+                    case 1:
+                        lampStatus[0] = !lampStatus[0];
+                        break;
+                    case 2:
+                        tempLamp = lampStatus[0];
+                        lampStatus[0] = lampStatus[1];
+                        lampStatus[1] = tempLamp;
+                        break;
+                    case 3:
+                        tempLamp = lampStatus[1];
+                        lampStatus[1] = lampStatus[2];
+                        lampStatus[2] = tempLamp;
+                        break;
+
+                }
+                
+                PrintLamps(lampStatus);
+                
+                foreach(bool lamp in lampStatus)
+                {
+                    if(lamp)
+                        lampsOn++; 
+                }
+
+                if (lampsOn == 3)
+                    victory = true;
+                else
+                    lampsOn = 0;
+            }
+
+            if(victory)
+                Console.WriteLine("You Win!");
+            else
+                Console.WriteLine("You Lose!");
+        }
+
+        static void PrintLamps(bool[] lamps)
+        {
+            //Instantiate String Builder
+            string output = "";
+
+            //Cycle through lamps
+            for (int i = 0; i < lamps.Length; i++)
+            {
+                //Add full square if lamp is on
+                if (lamps[i])
+                    output+= "True ";
+                else
+                    output+= "False ";
+            }
+
+            //Print the output
+            Console.WriteLine(output);
         }
     }
 }
