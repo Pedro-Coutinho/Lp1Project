b4db720383274df3b2c3e9d017ca04f34c9c11d1
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-04-13 16:20:06 +0100

    XML add on Input Handler

--- LampPuzzle/InputHandler.cs
+++ LampPuzzle/InputHandler.cs
@@ -4,6 +4,11 @@ namespace LampPuzzle
 {
     public class InputHandler
     {
+        /// <summary>
+        /// Method that verify input given by the user
+        /// Check it to see if is valid, if not, print that information
+        /// </summary>
+        /// <returns></returns> input
         public static int ReadInput()
         {
             int input = 0;
@@ -13,15 +18,15 @@ namespace LampPuzzle
             {
                 validInput = Int32.TryParse(Console.ReadLine(), out input);
 
-                if (validInput)
+                if (validInput)//if input is valid
                 {
-                    if (input != 1 && input != 2 && input != 3)
+                    if (input != 1 && input != 2 && input != 3)//condition check
                     {
-                        validInput = false;
+                        validInput = false;//input isn't: 1,2,3; input isn't valid
                     }
                 }
 
-                if (!validInput)
+                if (!validInput)//if input is not valid
                     Console.WriteLine("Invalid input");
             }
             while (!validInput);
